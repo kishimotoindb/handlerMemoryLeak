ce1e870bed18c7176937007e66741e6699c63410
cuihaichen@macpro <519376470@qq.com>
2017-08-04 17:40:46 +0800

    new Handler(new Callback(){}); handler.postDelay(new Runable(){})

--- app/src/main/java/com/example/bear/handlermemoryleak/NoViewInHandler/NewCallBackInHandlerConstructorActivity.java
+++ app/src/main/java/com/example/bear/handlermemoryleak/NoViewInHandler/NewCallBackInHandlerConstructorActivity.java
@@ -9,6 +9,7 @@ import com.example.bear.handlermemoryleak.R;
 
 /*
    1.会内存泄漏的情况：
+   1)CallBack使用匿名内部类
    Handler mHandler1 = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
@@ -20,6 +21,16 @@ import com.example.bear.handlermemoryleak.R;
 
     因为callback持有Activity，handler持有callback，message持有handler，messageQueue持有message，
     所以即使callback中不包含对View进行处理，也会因为持有外部类引用，而造成Activity的内存泄漏。
+
+    2)Runnable使用匿名内部类
+    mHandler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 3600 * 1000);
+    与1)是同样的道理，因为Runnable使用了匿名内部类，所以会持有外部Activity的引用，Runnable以callback
+    的形式保存在message中，message在messageQueue中，所以Activity泄漏
 
     2.不会发生内存泄漏的情况：
     Handler mHandler2 = new Handler(new MyCallBack());
@@ -46,14 +57,27 @@ public class NewCallBackInHandlerConstructorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_call_back_in_handler_constructor);
 //        mHandler1.sendEmptyMessageDelayed(1, 3600000);
-        mHandler2.sendEmptyMessageDelayed(1, 3600 * 1000);
+//        mHandler2.sendEmptyMessageDelayed(1, 3600 * 1000);
+
+        mHandler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 3600 * 1000);
     }
 
     static class MyCallBack implements Handler.Callback {
         @Override
         public boolean handleMessage(Message msg) {
+            Runnable callback = msg.getCallback();
+            callback.run();
             return false;
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
